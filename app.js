c8d9eac1863ac81f333e1fb9b3ea6787efcda302
Louise Norris <louisenorris73@yahoo.co.uk>
2020-05-29 15:57:33 +0100

    added functionality to access device camera and take photo

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+const supported = 'mediaDevices' in navigator;
+
 const cameraButton = document.getElementById('camera__button');
 const photoFrame = document.querySelector('.photo-frame');
 const photo = document.querySelector('.photo');
+const player = document.getElementById('player');
+const context = photo.getContext('2d');
 
 const showPhoto = () => {
     photoFrame.classList.add('show');
@@ -8,3 +12,18 @@ const showPhoto = () => {
 }
 
 cameraButton.addEventListener('click', showPhoto);
+
+const constraints = {
+    video: true,
+  };
+
+cameraButton.addEventListener('click', () => {
+// Draw the video frame to the canvas.
+    context.drawImage(player, 0, 0, photo.width, photo.height);
+});
+
+// Attach the video stream to the video element and autoplay.
+navigator.mediaDevices.getUserMedia(constraints)
+    .then((stream) => {
+      player.srcObject = stream;
+    });
